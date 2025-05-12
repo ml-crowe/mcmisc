9c6e950e57e8729b7c3af7e474d73073834ac8be
Michael Crowe <michael.l.crowe@gmail.com>
2025-05-12 10:39:55 -0400

    renamed mean.n mean_n to be more in line with naming conventions and avoid mislabeling of function as S3 method.

--- R/mean_n.R
+++ R/mean_n.R
@@ -6,7 +6,7 @@
 #' a specified tolerance (`n`) for the number of `NA` values per row.
 #'
 #' For example, if you have a 15-item scale and a participant completed only 14,
-#' `rowMeans()` would yield `NA` for that participant, but `mean.n()` can
+#' `rowMeans()` would yield `NA` for that participant, but `mean_n()` can
 #' still return the mean if the number of missing items is less than or equal to `n`.
 #'
 #' @param df A data frame or matrix of numeric variables (rows = observations, columns = items).
@@ -20,11 +20,11 @@
 #'   item2 = c(2, NA, NA, 4),
 #'   item3 = c(3, 4, 4, NA)
 #' )
-#' mean.n(df, n = 1)
+#' mean_n(df, n = 1)
 #' #> [1] 2 3 NA 4
 #'
-#' @export mean.n
-mean.n <- function(df, n) {
+#' @export mean_n
+mean_n <- function(df, n) {
   means <- apply(as.matrix(df), 1, mean, na.rm = TRUE)
   notvalid <- apply(as.matrix(df), 1, function(row) sum(is.na(row)))
   ifelse(notvalid <= n, means, NA)
